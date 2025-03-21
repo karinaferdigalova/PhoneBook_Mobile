92ddc93792964f942f0a5d49243da9398fc68898
Karina <karinaza292002@gmail.com>
2025-03-21 13:06:57 +0200

    add config and launch test

---
+++ src/test/java/tests/LaunchTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import config.AppiumConfig;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import screens.SplashScreen;
+
+public class LaunchTest extends AppiumConfig {
+    @Test
+    public void launch(){
+       String version = new SplashScreen(driver).getCurrentVersion();// version 1.0.0
+        Assert.assertTrue(version.contains("1.0.0"));
+
+
+
+    }
+}
